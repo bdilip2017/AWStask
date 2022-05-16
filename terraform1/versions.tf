f428de14a0d8db882da670d898c19756dce90c56
bdilip2017 <bdildhiraj@gmail.com>
2022-05-16 13:43:02 +0530

    Add files via upload

---
+++ terraform1/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.12.26"
+
+  backend "s3" {
+    bucket = "awstaskterraformdilip"
+    key    = "key1"
+    region = "ap-south-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.20"
+    }
+  }
+}
+
+provider "aws" {
+ 	access_key = "AKIAZW4ULLTM4M2QH2BO"
+	secret_key = "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"
+ 	region = "ap-south-1"
+}
